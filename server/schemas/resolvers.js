20f0ebab873e5444209250d11588a2fa138f0225
Marcus Ikegami <hikegami.6@gmail.com>
2022-10-17 16:33:51 -0600

    changed heroku folder

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -52,7 +52,7 @@ const resolvers = {
                 const { createReadStream, filename, mimetype, encoding } = await file;
                 const stream = createReadStream();
                 console.log('directory name', __dirname);
-                const pathName = path.join(__dirname, `../client/public/assets/${filename}`);
+                const pathName = path.join(__dirname, `/client/public/assets/${filename}`);
                 await stream.pipe(fs.createWriteStream(pathName));
                 const Upload = await File.create({filename: filename, url: pathName});
             }
