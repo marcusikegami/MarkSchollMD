63914315465d09c3bbf91538faa1bfc40e4e824a
Marcus Ikegami <mikegami.github@gmail.com>
2023-02-07 13:20:28 -0700

    upload redundant changes from marcu2

---
+++ server/schemas/listBuckets.js
@@ -0,0 +1,16 @@
+// Load the AWS SDK for Node.js
+import AWS from 'aws-sdk';
+// Set the region 
+AWS.config.update({region: 'us-west-2'});
+
+// Create S3 service object
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Call S3 to list the buckets
+s3.listBuckets(function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Buckets);
+  }
+});
